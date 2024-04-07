29de7104b5bd9194f7f183973637b5f71672690d
Muhammad Talha <mtalha0574758@gmail.com>
2024-04-07 13:21:28 -0700

    "Initialized simple currency converter project"

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Currency Converter
+
+Simple Currency Converter is a command-line tool built with TypeScript, Node.js, inquirer, and chalk.
---
+++ index.js
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const currency = {
+    USD: 1, //base currency
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280,
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: chalk.rgb(15, 100, 204)("Enter From Currency"),
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+    },
+    {
+        name: "to",
+        message: chalk.ansi256(196)("Enter To Currency"),
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+    },
+    {
+        name: "amount",
+        message: chalk.ansi256(190)("Enter Your Amount"),
+        type: "number",
+    },
+]);
+let fromAmount = currency[user_answer.from]; // exchange rate
+let toAmount = currency[user_answer.to]; // exchange rate
+let amount = user_answer.amount;
+let baseAmount = amount / fromAmount; //USD base currency
+let convertedAmount = baseAmount * toAmount;
+console.log(chalk.ansi256(198)(convertedAmount.toFixed(2)));
